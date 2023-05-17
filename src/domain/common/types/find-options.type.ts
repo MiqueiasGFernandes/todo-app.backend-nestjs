429ecf76ca87e8f49eb94d9f2cf9abbd8cdbed14
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-05-17 19:01:26 -0300

    fix: ensure filters is an mapped object

--- src/domain/common/types/find-options.type.ts
+++ src/domain/common/types/find-options.type.ts
@@ -2,5 +2,5 @@ export type FindOptionsType<T> = {
   page: number;
   perPage: number;
   order: { [K in keyof T]: 'ASC' | 'DESC' };
-  filters: { [K in keyof T]: string | number | boolean }[];
+  filters: { [K in keyof T]: string | number | boolean };
 };

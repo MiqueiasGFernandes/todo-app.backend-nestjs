d7eb27457430857c8f0810a2bacba06290e804e0
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-05-17 20:03:07 -0300

    fix: set order and filters as optional

--- src/domain/common/types/find-options.type.ts
+++ src/domain/common/types/find-options.type.ts
@@ -1,6 +1,6 @@
 export type FindOptionsType<T> = {
   page: number;
   perPage: number;
-  order: { [K in keyof T]: 'ASC' | 'DESC' };
-  filters: { [K in keyof T]: string | number | boolean };
+  order: { [K in keyof T]?: 'ASC' | 'DESC' };
+  filters: { [K in keyof T]?: string | number | boolean };
 };
